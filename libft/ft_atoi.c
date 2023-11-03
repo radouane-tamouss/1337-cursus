84d03a651b988877e6fa9c7c49aac32f29166ba3
Radouane Tamouss <rtamouss@e1r1p3.1337.ma>
2023-11-03 12:01:58 +0100

    push

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,34 @@
 /*   By: rtamouss <rtamouss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 18:30:48 by rtamouss          #+#    #+#             */
-/*   Updated: 2023/11/01 18:30:50 by rtamouss         ###   ########.fr       */
+/*   Updated: 2023/11/03 12:01:49 by rtamouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	sign;
+	int	res;
+
+	i = 0;
+	res = 0;
+	sign = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = (res * 10) + (str[i] - 48);
+		i++;
+	}
+	res *= sign;
+	return (res);
+}
