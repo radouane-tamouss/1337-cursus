1940d8b3c96a7d1ecd66feb7fcb0dd5825e6ec1d
Radouane Tamouss <rtamouss@e2r12p7.1337.ma>
2023-11-04 21:53:28 +0100

    push

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -1,22 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rtamouss <rtamouss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/02 17:52:19 by rtamouss          #+#    #+#             */
-/*   Updated: 2023/11/04 21:44:52 by rtamouss         ###   ########.fr       */
+/*   Created: 2023/11/04 20:59:51 by rtamouss          #+#    #+#             */
+/*   Updated: 2023/11/04 21:40:26 by rtamouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stdlib.h>
+void	ft_bzero(void *s, size_t n)
+{
+	int	c;
 
-size_t	ft_strlen(char *s);
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
+	c = 0;
+	ft_memset(s, c, n);
+}
 
-#endif
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+	char h[5] = "hello";
+
+	ft_bzero(h,sizeof(h));
+	size_t i = 0;
+	while (i < sizeof(h))
+	{
+		printf("|%x|",h[i]);
+		i++;
+	}
+}
