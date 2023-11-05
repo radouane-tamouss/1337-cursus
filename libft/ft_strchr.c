aff10732e464430a7e4c229d20e413816494536e
Radouane Tamouss <rtamouss@e2r10p9.1337.ma>
2023-11-05 15:14:41 +0100

    push o ssf

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,8 +6,36 @@
 /*   By: rtamouss <rtamouss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 00:40:02 by rtamouss          #+#    #+#             */
-/*   Updated: 2023/11/05 02:53:13 by rtamouss         ###   ########.fr       */
+/*   Updated: 2023/11/05 15:14:36 by rtamouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (const char)c)
+			return ((char *)(s + i));
+		i++;
+	}
+	if (c == '\0')
+		return ((char *)(s + i));
+	return (NULL);
+}
+
+// #include <stdio.h>
+// #include <string.h>
+// int		main(void)
+// {
+// 	char *s = "hello";
+// 	char *k = "hello";
+// 	printf("located pointer : %p\nvalue : %s\n", ft_strchr(s,'\0')
+//,ft_strchr(s,'\0'));
+// 	printf("located pointer : %p\nvalue : %s\n", strchr(k,'\0'),strchr(k,'\0'));
+// 	return (0);
+// }
