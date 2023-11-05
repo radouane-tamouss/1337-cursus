ae1a7e6bd96adce20b8e4fb405cb680019a68125
Radouane Tamouss <rtamouss@e2r10p9.1337.ma>
2023-11-05 22:13:25 +0100

    push o ssf

---
+++ libft/ft_strnstr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rtamouss <rtamouss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/05 18:43:24 by rtamouss          #+#    #+#             */
+/*   Updated: 2023/11/05 22:13:17 by rtamouss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (needle[j] == '\0' && len == 0)
+		return ((char *)haystack);
+	else if (!len)
+		return (NULL);
+	else if (needle[j] == '\0')
+		return ((char *)haystack);
+	while (haystack[i] && i < len)
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j] && i + j < len)
+		{
+			if (needle[j + 1] == '\0')
+				return ((char *)&haystack[i]);
+			j++;
+		}
+	i++;
+	}
+	return (NULL);
+}
