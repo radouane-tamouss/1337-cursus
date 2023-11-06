57bcd8de4c15a682c17c4ef2b6a7a3bc8d42520b
Radouane Tamouss <rtamouss@e1r3p10.1337.ma>
2023-11-06 15:35:55 +0100

    push o ssf

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rtamouss <rtamouss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 14:23:28 by rtamouss          #+#    #+#             */
+/*   Updated: 2023/11/06 15:35:36 by rtamouss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*s3;
+
+	i = 0;
+	j = 0;
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	s3 = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (s3 == NULL)
+		return (NULL);
+	while (s1[i])
+	{
+		s3[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		s3[i] = s2[j];
+		i++;
+		j++;
+	}
+	s3[i] = '\0';
+	return (s3);
+}
+
+// #include <stdio.h>
+// #include <unistd.h>
+// int	main(void)
+// {
+// 	char	*s1 = "NULL";
+// 	char	*s2 = "hello";
+// 	char	*s3;
+// 	s3 = ft_strjoin(s1,s2);
+// 	printf("%s", s3);
+// 	return (0);
+// }
