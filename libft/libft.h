b019eb2b88eeddd2486c839acfa740e5675d02f9
Radouane Tamouss <rtamouss@e1r3p10.1337.ma>
2023-11-14 01:48:31 +0100

    push o ssf

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: rtamouss <rtamouss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 17:52:19 by rtamouss          #+#    #+#             */
-/*   Updated: 2023/11/14 01:37:22 by rtamouss         ###   ########.fr       */
+/*   Updated: 2023/11/14 01:45:51 by rtamouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,5 +65,6 @@ t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
