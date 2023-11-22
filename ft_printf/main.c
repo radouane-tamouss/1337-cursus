7fbd7684eb2680e66e3fde3747107b9cc8076719
Radouane Tamouss <atamousse.red@gmail.com>
2023-11-22 22:57:20 +0100

    .

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -6,7 +6,7 @@
 /*   By: rtamouss <rtamouss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 16:13:57 by rtamouss          #+#    #+#             */
-/*   Updated: 2023/11/22 22:56:30 by rtamouss         ###   ########.fr       */
+/*   Updated: 2023/11/22 22:57:13 by rtamouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ int main(void)
 	int d = ft_printf(NULL);
 	// int d2 = printf(NULL);
 	// int d2 = 0;
-	printf("%d", d);
+	printf("%d\n", d);
     return 0;
 }
